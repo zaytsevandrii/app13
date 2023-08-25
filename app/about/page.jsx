16611ae931d967f5e55a54d53bf94dc3a1510250
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-08-25 15:08:46 +0200

    add custum layout

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>Welcome to About page</h1>
+            
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>Our team</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, corrupti eaque quaerat facere expedita corporis. Aliquid voluptatem aut odio cupiditate odit debitis, quae fuga autem? Quasi sint similique voluptatibus molestiae?</p>
+    </div>
+  )
+}
+
+export default page
